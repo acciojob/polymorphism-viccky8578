cd0c919be8fff2379c45ea75b7cc8385fc289e63
viccky8578 <manbodhvikram@gmail.com>
2023-02-10 00:33:43 +0530

    code of polymerphism

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,29 @@
 package com.driver;
 
 public class Main {
+    public static class Product {
+        public int product(int x, int y) {
+            return x * y;
+        }
 
+        public int product(int x, int y, int z) {
+            return x * y * z;
+        }
+
+        public double product(double x, double y) {
+            return x * y;
+        }
+
+
+        public static void main(String[] args) {
+            Product p = new Product();
+            p.product(5, 5);
+
+            p.product(2, 3, 4);
+            p.product(2.2, 2.3);
+            //System.out.println(p.product(2,3));
+
+        }
+
+    }
 }
